50c7207bdab08ea8d77898eb447d74f207f87b6a
Faisal <hayat.khanum92@gmail.com>
2024-10-27 12:36:55 +0530

    Changes added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 //         Uncomment this block to pass the first stage
+        List<String> arguments = Arrays.asList("echo", "type", "exit");
         while (true) {
             System.out.print("$ ");
 
@@ -12,6 +16,12 @@ public class Main {
                 break;
             } else if (input.startsWith("echo ")) {
                 System.out.println(input.substring(5));
+            } else if (input.startsWith("type ")) {
+                if (arguments.contains(input.substring(5))) {
+                    System.out.println(input.substring(5) + " is a shell builtin");
+                } else {
+                    System.out.println(input.substring(5) + ": not found");
+                }
             } else {
                 System.out.println(input + ": command not found");
             }
