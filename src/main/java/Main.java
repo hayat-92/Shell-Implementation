523c19c8795e264ca4a2df39c0bfd5ff8051dcf9
Faisal <hayat.khanum92@gmail.com>
2024-10-27 12:27:00 +0530

    Changes added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
-            if("0".equals(input)) {
+            if("exit 0".equals(input)) {
                 break;
             }
             System.out.println(input + ": command not found");
